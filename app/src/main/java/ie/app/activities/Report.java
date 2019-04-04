b04683c071ad1239f5e0861d613180260be4f495
sneezingcat191 <huynguyend191@gmail.com>
2019-04-04 10:39:41 +0700

    week 7 + 8

--- app/src/main/java/ie/app/activities/Report.java
+++ app/src/main/java/ie/app/activities/Report.java
@@ -1,49 +1,63 @@
 package ie.app.activities;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;import android.os.Bundle;
-import android.app.Activity;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.view.Menu;
+import android.widget.TextView;
+
+import java.util.List;
 
 import ie.app.R;
-public class Report extends AppCompatActivity
+import ie.app.models.Donation;
+
+class DonationAdapter extends ArrayAdapter<Donation>
+{
+    private Context context;
+    public List<Donation> donations;
+    public DonationAdapter(Context context, List<Donation> donations)
+    {
+        super(context, R.layout.row_donate, donations);
+        this.context = context;
+        this.donations = donations;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        LayoutInflater inflater = (LayoutInflater)
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.row_donate, parent, false);
+        Donation donation = donations.get(position);
+        TextView amountView = (TextView) view.findViewById(R.id.row_amount);
+        TextView methodView = (TextView) view.findViewById(R.id.row_method);
+        amountView.setText("$" + donation.amount);
+        methodView.setText(donation.method);
+        return view;
+    }
+    @Override
+    public int getCount()
+    {
+        return donations.size();
+    }
+}
+
+public class Report extends Base
 {
     ListView listView;
-    static final String[] numbers = new String[] {
-            "Amount, Pay method",
-            "10, Direct",
-            "100, PayPal",
-            "1000, Direct",
-            "10, PayPal",
-            "5000, PayPal"};
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
         listView = (ListView) findViewById(R.id.reportList);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numbers);
+        DonationAdapter adapter = new DonationAdapter(this, donations);
         listView.setAdapter(adapter);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_donate, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId())
-        {
-            case R.id.menuDonate : startActivity (new Intent(this, Donate.class));
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
