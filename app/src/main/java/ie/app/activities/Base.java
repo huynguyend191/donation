01057bf1a7a794c8fca643c3f86125bddbc86907
sneezingcat191 <huynguyend191@gmail.com>
2019-04-09 22:21:10 +0700

    week 9 -11

--- app/src/main/java/ie/app/activities/Base.java
+++ app/src/main/java/ie/app/activities/Base.java
@@ -1,76 +1,85 @@
 package ie.app.activities;
-
-import ie.app.models.Donation;
-
-
+/**
+ * Created by ddrohan on 22/10/2015.
+ */
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
+import android.os.Bundle;
 
-import android.widget.Toast;
 import ie.app.R;
-import java.util.List;
-import java.util.ArrayList;
+import ie.app.main.DonationApp;
 
-public class Base extends AppCompatActivity
-{
-    public final int target = 10000;
-    public int totalDonated = 0;
-    public static List <Donation> donations = new ArrayList<Donation>();
+public class Base extends AppCompatActivity {
 
-    public boolean newDonation(Donation donation)
-    {
-        boolean targetAchieved = totalDonated > target;
-        if (!targetAchieved)
-        {
-            donations.add(donation);
-            totalDonated += donation.amount;
-        }
-        else
-        {
-            Toast toast = Toast.makeText(this, "Target Exceeded!", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-        return targetAchieved;
+    public DonationApp app;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        app = (DonationApp) getApplication();
+
+        app.dbManager.open();
+        app.dbManager.setTotalDonated(this);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        app.dbManager.close();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.menu_donate, menu);
         return true;
     }
+
     @Override
     public boolean onPrepareOptionsMenu (Menu menu){
         super.onPrepareOptionsMenu(menu);
         MenuItem report = menu.findItem(R.id.menuReport);
         MenuItem donate = menu.findItem(R.id.menuDonate);
-        if(donations.isEmpty())
+        MenuItem reset = menu.findItem(R.id.menuReset);
+
+        if(app.dbManager.getAll().isEmpty())
+        {
             report.setEnabled(false);
-        else
+            reset.setEnabled(false);
+        }
+        else {
             report.setEnabled(true);
+            reset.setEnabled(true);
+        }
         if(this instanceof Donate){
             donate.setVisible(false);
-            if(!donations.isEmpty())
+            if(!app.dbManager.getAll().isEmpty())
+            {
                 report.setVisible(true);
+                reset.setEnabled(true);
+            }
         }
         else {
             report.setVisible(false);
             donate.setVisible(true);
+            reset.setVisible(false);
         }
+
         return true;
     }
-    public void settings(MenuItem item)
-    {
-        Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show();
-    }
+
     public void report(MenuItem item)
     {
         startActivity (new Intent(this, Report.class));
     }
+
     public void donate(MenuItem item)
     {
         startActivity (new Intent(this, Donate.class));
     }
+
+    public void reset(MenuItem item) {}
 }
---
+++ app/src/main/java/ie/app/main/DonationApp.java
@@ -0,0 +1,41 @@
+package ie.app.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Application;
+import android.util.Log;
+import android.widget.Toast;
+
+import ie.app.models.Donation;
+
+public class DonationApp extends Application
+{
+    public final int       target       = 10000;
+    public int             totalDonated = 0;
+    public List <Donation> donations    = new ArrayList<Donation>();
+
+
+    public boolean newDonation(Donation donation)
+    {
+        boolean targetAchieved = totalDonated > target;
+        if (!targetAchieved)
+        {
+            totalDonated += donation.amount;
+        }
+        else
+        {
+            Toast.makeText(this, "Target Exceeded!", Toast.LENGTH_SHORT).show();
+        }
+        return targetAchieved;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        Log.v("Donate", "Donation App Started");
+        Log.v("Donate", "Database Created");
+
+    }
+}
